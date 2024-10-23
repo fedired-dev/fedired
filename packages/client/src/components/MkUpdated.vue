63a8fbb74e148bbb0b9047f110421c3f256a0d3d
srnovus <contacto.javieroficial@gmail.com>
2024-10-22 23:05:17 -0600

    “declaracion”

--- packages/client/src/components/MkUpdated.vue
+++ packages/client/src/components/MkUpdated.vue
@@ -7,9 +7,9 @@
 	>
 		<div :class="$style.root">
 			<div :class="$style.title">
-				<MkSparkle>{{ i18n.ts.misskeyUpdated }}</MkSparkle>
+				<MkSparkle>{{ i18n.ts.fediredUpdated }}</MkSparkle>
 			</div>
-			<div :class="$style.version">✨ {{ version }} 🚀</div>
+			<div :class="$style.version"> {{ version }} </div>
 			<MkButton
 				:class="$style.gotIt"
 				primary
@@ -17,6 +17,11 @@
 				@click="modal!.close()"
 				>{{ i18n.ts.gotIt }}</MkButton
 			>
+			<a :href="releaseUrl" target="_blank" rel="noopener noreferrer">
+				<MkButton :class="$style.releaseButton" secondary full>
+					{{ i18n.ts.viewRelease }}
+				</MkButton>
+			</a>
 		</div>
 	</MkModal>
 </template>
@@ -30,6 +35,7 @@ import { version } from "@/config";
 import { i18n } from "@/i18n";
 
 const modal = shallowRef<InstanceType<typeof MkModal>>();
+const releaseUrl = 'https://github.com/fedired-dev/fedired/releases/latest'; 
 </script>
 
 <style lang="scss" module>
@@ -59,5 +65,9 @@ const modal = shallowRef<InstanceType<typeof MkModal>>();
 	margin-inline-end: 0;
 	margin-block-end: 0;
 	margin-inline-start: 0;
+}
+
+.releaseButton {
+	margin-top: 8px;
 }
 </style>
