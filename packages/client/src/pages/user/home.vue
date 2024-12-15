535fd1c608cbc644e8c5c5ca942bcd678e1ff524
Javier Caceres <novus@MacBook-Pro-de-Javier.local>
2024-12-15 14:30:15 -0600

    correct v vue

--- packages/client/src/pages/user/home.vue
+++ packages/client/src/pages/user/home.vue
@@ -369,7 +369,6 @@ import { defaultStore } from "@/store";
 import { i18n } from "@/i18n";
 import { isModerator, isSignedIn, me } from "@/me";
 import icon from "@/scripts/icon";
-import { ref, onMounted } from 'vue';
 
 
 const XPhotos = defineAsyncComponent(() => import("./index.photos.vue"));
