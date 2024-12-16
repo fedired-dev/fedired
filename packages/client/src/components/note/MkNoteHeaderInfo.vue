67cf40e62183d28446d20ec27d347b751abf1d1d
Javier Caceres <novus@MacBook-Pro-de-Javier.local>
2024-12-15 19:00:54 -0600

    update code verifeid error

--- packages/client/src/components/note/MkNoteHeaderInfo.vue
+++ packages/client/src/components/note/MkNoteHeaderInfo.vue
@@ -21,12 +21,12 @@
 		<MkUserName class="username" :user="appearNote.reply.user" />
 
 		<span 
-										v-if="isVerified(user.username)" 
-										v-tooltip.noDelay="'Usuario Verificado'" 
-										class="verified-badge">
-										
-										<img src="https://raw.githubusercontent.com/fedired-dev/img/refs/heads/main/back/verifeid.png" width="20" height="20" alt="Verificado" />
-									</span>
+						v-if="isVerified(note.user.username)" 
+						v-tooltip.noDelay="'Usuario Verificado'" 
+						class="verified-badge"
+					>
+					<img src="https://raw.githubusercontent.com/fedired-dev/img/refs/heads/main/back/verifeid.png" width="20" height="20" alt="Verificado" />
+				</span>
 
 		<Mfm
 			class="summary"
