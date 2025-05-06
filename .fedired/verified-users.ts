4e4c86a85f42e54d01916dc4389f5c618fb1281b
srnovus <contacto.javieroficial@gmail.com>
2025-05-06 15:01:13 -0600

    update code api url and locations

--- .fedired/verified-users.ts
+++ .fedired/verified-users.ts
@@ -1,13 +1,6 @@
 const API_URL = "https://api-verify.fedired.com/verified-users";
 
 export const fetchVerifiedUsers = async (): Promise<string[]> => {
- 
-  const origin = window.location.origin;
-
-  if (!origin.includes("fedired.com")) {
-    throw new Error("Access denied. This action can only be performed from fedired.com");
-  }
-
   const res = await fetch(API_URL);
   const data = await res.json();
   return data.verifiedUsers;
